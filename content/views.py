3adb37cc00f20cc2ec13e0b4806f35dd66e58986
kbm1933 <wjsalsgh456@gmail.com>
2022-10-05 14:00:54 +0900

    컨텐츠 수정 기능 추가 x -> 컨텐츠 수정 기능 작동 확인

--- content/views.py
+++ content/views.py
@@ -1,9 +1,10 @@
-from content.models import ContentModel, ContentComment, ContentModify
+from content.models import ContentModel, ContentComment, UserModel
+from .models import Photo
 from django.views.generic import ListView, TemplateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-#
+
 # Create your views here.
 def home(request):
     #로그인이 되어 있다면
@@ -26,8 +27,9 @@ def content(request):
     if request.method == 'GET':
         user = request.user.is_authenticated
         if user:#로그인이 되어 있다면
+            user_list = UserModel.objects.all().exclude(username = request.user.username)
             all_content = ContentModel.objects.all().order_by('-created_at')
-            return render(request,'content/home.html',{'content':all_content})
+            return render(request,'content/home.html',{'content':all_content, 'user_list':user_list})
         else:#로그인이 안되어 있다면
             return redirect('/sign-in')
 
@@ -41,6 +43,14 @@ def content(request):
         else:
             my_content = ContentModel.objects.create(author=user,contents=contents)
             my_content.save()
+
+            # 이미지 추가
+            for img in request.FILES.getlist('imgs'):
+                photo = Photo()
+                photo.post = my_content
+                photo.image = img
+                photo.save()
+
             return redirect('/content')
 
 @login_required
@@ -58,8 +68,10 @@ def delete_content(request,id):
 @login_required
 def detail_content(request,id):
     my_content = ContentModel.objects.get(id=id)
-    content_comment = ContentModel.objects.filter(contents = id).order_by('-created_at')
-    return render(request,'content/content_detail.html',{'content':my_content,'comment':content_comment})
+    user_list = UserModel.objects.all().exclude(username = request.user.username)
+    content_comment = ContentComment.objects.filter(contents = id).order_by('-created_at')
+    return render(request,'content/content_detail.html',{'content':my_content,
+    'comment':content_comment,'user_list':user_list})
 
 @login_required
 def write_comment(request,id):
@@ -83,25 +95,51 @@ def delete_comment(request,id):
     return redirect('/content/'+str(current_content))
 
 @login_required
-def modify_content(request,id):
-    # my_content = ContentModel.objects.get(id=id)
-    # # content_modify = ContentModel.objects.filter(contents=id).order_by('-created_at')
-    # if request.method == 'POST':
-    #     contents = request.POST.get('contents')
-    #     if my_content is not None:
-    #         my_content.contents = contents
-    #         my_content.save()
-    #         return redirect('/content')
-    #
-    # return render(request, 'content/content_modify.html', {'content': my_content})#,'modify-content':content_modify})
+def modify_content(request, id):
     my_content = ContentModel.objects.get(id=id)
-    content_modify = ContentModel.objects.filter(contents=id).order_by('-created_at')
     if request.method == 'POST':
-        contents = request.POST.get('contents')
-        my_content.contents = contents
+        my_content.contents = request.POST['my-content']
         my_content.save()
+        return redirect('/content')
 
-    return render(request, 'content/content_modify.html', {'content': my_content, 'modify-content': content_modify})
+    return render(request, 'content/content_modify.html', {'content': my_content})
 
 
 
+
+# 좋아요버튼-메인페이지
+@login_required
+def likes(request, id):
+    me = request.user
+    click_content = ContentModel.objects.get(id=id)
+    if me in click_content.liked.all():
+        click_content.liked.remove(me)
+    else:
+        click_content.liked.add(me)
+    return redirect('/')
+
+# 좋아요버튼 - 디테일페이지
+@login_required
+def likes_detail(request, id):
+    me = request.user
+    click_content = ContentModel.objects.get(id=id)
+    if me in click_content.liked.all():
+        click_content.liked.remove(me)
+    else:
+        click_content.liked.add(me)
+    return redirect('/content/'+str(id))
+
+@login_required
+def like_content(request):
+    if request.method == 'GET':  # 요청하는 방식이 GET 방식인지 확인하기
+        user = request.user.is_authenticated
+        if user:
+            user_list = UserModel.objects.all().exclude(username = request.user.username)
+            all_content = ContentModel.objects.all().order_by('-created_at')
+            context = {
+                'content':all_content,
+                'user_list':user_list,
+            }
+            return render(request,'content/like_content.html',context)
+        else:
+            return redirect('/sign-in')
